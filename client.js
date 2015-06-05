803a7859b624f134ec793987c72fec2425c7f515
Abhinav Rastogi <r.abhinav@gmail.com>
2015-06-05 16:48:30 +0530

    basic orientation and motion data passed from client to viewer

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
 var socket = io();
+
+if(window.location.pathname=='/') {
+  window.addEventListener('deviceorientation', function(event) {
+    socket.emit('deviceorientation', {
+      alpha: event.alpha,
+      beta: event.beta,
+      gamma: event.gamma
+    });
+  });
+  window.addEventListener('devicemotion', function(event) {
+    socket.emit('devicemotion', event.accelerationIncludingGravity);
+  });
+};
+
+if(window.location.pathname=='/view') {
+  socket.on('deviceorientation', function(data) {
+    console.log(data);
+  });
+};
--- server.js
+++ server.js
@@ -10,8 +10,19 @@ app.get('/', function(req, res) {
 	res.end(fs.readFileSync('index.html'));
 });
 
+app.get('/view', function(req, res) {
+	res.end(fs.readFileSync('view.html'));
+});
+
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log('a user connected', socket.id);
+
+	socket.on('deviceorientation', function(data) {
+		socket.broadcast.emit('deviceorientation', data);
+	});
+	socket.on('devicemotion', function(data) {
+		socket.broadcast.emit('devicemotion', data);
+	});
 });
 
 http.listen(3000, function(err) {
