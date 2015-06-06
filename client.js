072a94b31d65eb97da0b43d812fcbcfe286fe486
Boopathi Rajaa <me@boopathi.in>
2015-06-06 13:41:50 +0530

    window resize

--- client.js
+++ client.js
@@ -378,6 +378,14 @@ if(window.location.pathname=='/view') {
     addBox(posx, posy, posz);
   });
 
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  }
+
   // var box = document.getElementById('box');
   // socket.on('deviceorientation', function(data) {
   //   // console.log(data);
